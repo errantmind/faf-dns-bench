32439262cdc6446ca3bd6f47d4ad896342006922
errantmind <errantmind@protonmail.com>
2023-02-10 16:45:14 -0600

    Squashed commit of the following:
    commit cf7888a310d78ffe34a938befbfd6cb609d5524b
    Author: errantmind <errantmind@protonmail.com>
    Date:   Fri Feb 10 16:42:14 2023 -0600
    
        fix polling bug related from lack of loop
    
    commit a193c2a1486adb370ff371b964dd26d0e7f513f0
    Author: errantmind <errantmind@protonmail.com>
    Date:   Fri Feb 10 16:17:29 2023 -0600
    
        testing MIO for async instead of raw EPOLL

--- src/statics.rs
+++ src/statics.rs
@@ -23,7 +23,5 @@ pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 pub static ARGS: once_cell::sync::Lazy<crate::args::Args> = once_cell::sync::Lazy::new(clap::Parser::parse);
 
 pub const DOMAINS_TO_INCLUDE: usize = 250;
-pub const MAX_EPOLL_EVENTS_RETURNED: isize = 340;
-pub const EPOLL_TIMEOUT_MILLIS: isize = 1000;
 pub const MAX_CONCURRENCY: usize = 8;
 pub const COLLECTION_TIMEOUT_MS: i64 = 2000;
